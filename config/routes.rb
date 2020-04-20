e1c3665d2c919b4371b86c6b8d5d4f4b76c061a2
Mahsa Khamisi <mahsa.khamisi@me.com>
2020-04-20 08:33:56 +0200

    schema and models, relations and routes Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'pages#home'
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :businesses, only: [:index]
 
-  root to: 'pages#home'
 
-  devise_for :users, path: 'users', controllers: {
-    sessions: "users/sessions"
-  }
+
+  # devise_for :users, path: 'users', controllers: {
+  #   sessions: "users/sessions"
+  # }
 
   resources :businesses, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     resources :business_offers, only: [:new, :create, :show, :edit, :update, :destroy]
@@ -24,6 +25,9 @@ Rails.application.routes.draw do
 
   get "/businesses/:user_id/view_orders", to: "businesses#view_orders", as: "view_business_orders"
   get "/businesses/:user_id/view_hisotry", to: "businesses#view_hisotry", as: "view_business_hisotry"
+
+
+
   # get "/businesses/:user_id/withdraw", to: "businesses#withdraw", as: "withdraw"
   # get "/orders/:user_id/transactions", to: "orders#view_history", as: "view_user_history"
   # get "/orders/:user_id/view_all", to: "orders#view_all", as: "view_user_orders"
--- db/migrate/20200419203355_create_order_items.rb
+++ db/migrate/20200419203355_create_order_items.rb
@@ -2,7 +2,7 @@ class CreateOrderItems < ActiveRecord::Migration[6.0]
   def change
     create_table :order_items do |t|
       t.integer :quantity
-      t.references :BusinessOffer, null: false, foreign_key: true
+      t.references :business_offer, null: false, foreign_key: true
 
       t.timestamps
     end
