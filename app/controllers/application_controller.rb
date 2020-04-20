f5fabb750a229318184c09bebdce1f48a5db4210
Andrea Tota <andrea.tota6@gmail.com>
2020-04-20 09:10:06 +0200

    Add devise gem to Gemfile. Create User model with Devise. Create skeleton for navbar as partial file and render it for all pages. Run migrations for users table to add first_name, last_name and 'owner' status as boolean

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, except: :home
+
+  protected
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    # devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password])
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit({ owner: [] }, :first_name, :last_name, :email, :password, :password_confirmation)
+    end
+
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
