262c068adb1d53db67f8e7479326238c3a3e052d
Mahsa Khamisi <mahsa.khamisi@me.com>
2020-04-20 14:44:22 +0200

    done

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   # belongs_to :order_item
-  has_many :order_items
+  has_many :order_items, dependent: :destroy
   belongs_to :user
 end
 
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,5 +1,5 @@
 class OrderItem < ApplicationRecord
   belongs_to :business_offer
   belongs_to :business, through: :business_offer
-  belongs_to :order
+  belongs_to :order, dependent: :destroy
 end
