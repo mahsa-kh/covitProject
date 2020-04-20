5e4399c53bc017b5e10b07eefda7ad9212b10601
Mahsa Khamisi <mahsa.khamisi@me.com>
2020-04-20 14:20:19 +0200

    Done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_080332) do
+ActiveRecord::Schema.define(version: 2020_04_20_121842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 2020_04_20_080332) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "order_id", null: false
+    t.boolean "gift"
+    t.text "gift_email"
     t.index ["business_offer_id"], name: "index_order_items_on_business_offer_id"
     t.index ["order_id"], name: "index_order_items_on_order_id"
   end
