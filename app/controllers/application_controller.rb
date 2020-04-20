07446e8d4dc72f9956b055d24c29ee160e6416f1
Andrea Tota <andrea.tota6@gmail.com>
2020-04-20 19:29:59 +0200

    Create logic for SignIn as user: a user who is owner of a business will be redirected to his business; if he/she doesn't have one yet will be redirected to the creation of the new business. If it's a normal user will be redirected to the homepage.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
 
-
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!, except: :home
 
@@ -10,7 +9,7 @@ class ApplicationController < ActionController::Base
     # For additional fields in app/views/devise/registrations/new.html.erb
     # devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password])
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit({ owner: [] }, :first_name, :last_name, :email, :password, :password_confirmation)
+      user_params.permit(:first_name, :last_name, :email, :password, :password_confirmation, :owner)
     end
 
     devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
@@ -18,5 +17,20 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
 
+  end
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    # return the path based on resource
+    if resource.owner
+      # if resource.owner.businesses
+          return business_path(@business)
+      # else
+        return new_business_path
+      # end
+    else
+      return root_path
+    end
   end
 end
