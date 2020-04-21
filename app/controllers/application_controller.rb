7b4d5f20e4f4e095f6725ad3c246051d4627e3e7
Rodolfo Pinto <rodolfopintomail@gmail.com>
2020-04-21 15:41:18 +0200

    Business Offers new created: Controller and View

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
 
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!, except: :home
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :authenticate_user!, except: :home
 
   protected
 
